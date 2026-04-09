8ae034d0c1a3064a4d8cff1f06f5b853ddf79513
Pooya Parsa <pooya@pi0.io>
2026-04-09 10:34:47 +0200

    fix: fix vsda shim

--- shims/vsda/rust/web/vsda.js
+++ shims/vsda/rust/web/vsda.js
@@ -1,17 +1,20 @@
 // Mock vsda WASM browser module
-// VS Code expects a global `vsda_web` after loading this script.
-globalThis.vsda_web = {
-  default: async function (_bytes) {},
-  sign(value) {
-    return value;
-  },
-  validator: class {
-    createNewMessage(_msg) {
-      return "ok";
-    }
-    validate(_value) {
-      return "ok";
-    }
-    free() {}
-  },
-};
+define([], function () {
+  const vsda = {
+    default: async function (_bytes) {},
+    sign(value) {
+      return value;
+    },
+    validator: class {
+      createNewMessage(_msg) {
+        return "ok";
+      }
+      validate(_value) {
+        return "ok";
+      }
+      free() {}
+    },
+  };
+  globalThis.vsda_web = vsda;
+  return vsda;
+});
